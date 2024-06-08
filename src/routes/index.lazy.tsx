31178333b9054b6fdee75daccd3d2fcf9b24c9d2
konthon <konthon@hotmail.co.th>
2024-06-08 21:45:42 +0700

    feat: update homepage

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -2,10 +2,14 @@ import { Link, createLazyFileRoute } from '@tanstack/react-router'
 import { FC } from 'react'
 
 const Home: FC = () => (
-  <div className='px-4 md:container md:mx-auto'>
-    <h1 className='text-5xl font-bold'>Color tools</h1>
-    <Link to='/opacify'>Opacify</Link>
-  </div>
+  <main className='flex min-h-dvh flex-col justify-center px-4 md:container md:mx-auto'>
+    <div className='text-6xl'>🎨</div>
+    <h1 className='mb-12 text-3xl font-medium'>Color tools</h1>
+    <nav className='flex flex-col text-6xl font-bold [&_a:hover]:underline [&_a]:mb-6'>
+      <Link to='/opacify'>Opacify</Link>
+      <Link to='/theme/palette'>Generate Palette</Link>
+    </nav>
+  </main>
 )
 
 export const Route = createLazyFileRoute('/')({
